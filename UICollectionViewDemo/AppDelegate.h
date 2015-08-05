f41a7b80f5e7dce4ba146e7aaa8320997c14f75c
hedawei <2498178926@qq.com>
2015-08-05 11:08:12 +0800

    Initial Commit

---
+++ UICollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewDemo
+//
+//  Created by hedawei on 15/8/5.
+//  Copyright (c) 2015年 hedawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
